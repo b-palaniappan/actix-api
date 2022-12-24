043a80396bbd980e9a16ef7b391a26f8138cd9c7
Balasubramanian Palaniappan <b.palaniappan@hotmail.com>
2022-12-24 13:27:12 -0600

    starter actix main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-use nanoid::nanoid;
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
-fn main() {
-    let id = nanoid!();
-    println!("id - {}", id);
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().json("Hello from rust and mongoDB")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("localhost", 8080))?
+        .run()
+        .await
 }
