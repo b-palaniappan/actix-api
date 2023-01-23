0831206552871c76d703aaee2d1393c4f4f34734
Balasubramanian Palaniappan <b.palaniappan@hotmail.com>
2023-01-23 11:43:37 -0600

    cleanup and validate GitHub Actions

--- src/constants.rs
+++ src/constants.rs
@@ -1,7 +1,3 @@
-// Controller endpoints.
-pub const USERS_API_ENDPOINT: &str = "/users";
-pub const AUTH_API_ENDPOINT: &str = "/a";
-
 // MongoDB constants.
 pub const MONGO_DATABASE: &str = "rustDB";
 pub const MONGO_USER_COLLECTION: &str = "user";
